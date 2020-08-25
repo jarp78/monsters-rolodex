8efa5c424832d09b2ca876fe2e49b4bcb796f85a
jarp78 <jarp78@gmail.com>
2020-08-24 20:29:16 -0500

    Updates

---
+++ precache-manifest.19f7dce3e31786cd1f18b294088162ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e674ca17ec76afd1d873b9b381fc8a08",
+    "url": "/jarp78/monsters-rolodex.git/index.html"
+  },
+  {
+    "revision": "a97ab2a18a9821621469",
+    "url": "/jarp78/monsters-rolodex.git/static/css/main.9133a68e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/jarp78/monsters-rolodex.git/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jarp78/monsters-rolodex.git/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97ab2a18a9821621469",
+    "url": "/jarp78/monsters-rolodex.git/static/js/main.347772b9.chunk.js"
+  },
+  {
+    "revision": "ee895b98e54b228b9c6a",
+    "url": "/jarp78/monsters-rolodex.git/static/js/runtime-main.d3377bb2.js"
+  }
+]);
